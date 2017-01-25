633e6bdf64e69b1bbecd17f29cb8137443b1cf54
Erwin van der Koogh <erwin@bitgenics.io>
2017-01-25 14:45:11 +1100

    first cut

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const secret = new Buffer(process.env.JWT_SECRET, 'base64');
+const region = process.env.REGION || '*';
+const apiId = process.env.API_ID || '*';
+const stageId = process.env.STAGE_ID || '*';
+const method = process.env.METHOD || '*';
+const path = process.env.PATH || '*';
+
+const policy = {
+	Version: '2012-10-17',
+	Statement: {
+		Action: 'execute-api:Invoke',
+		Effect: 'Allow',
+		Resource: `arn:aws:execute-api:${region}:*:${apiId}/${stageId}/${method}/${path}`
+	}
+};
+
+module.exports.handler = function( event, context, callback ) {
+	if(event.type.toUpperCase() !== 'TOKEN') return callback('Authorisation not of type TOKEN');
+	if(!event.authorizationToken || !event.authorizationToken.startsWith('Bearer') ) return callback('No (proper) Authorization header');
+	const token = event.authorizationToken.replace(/^Bearer\s*/i, '');
+	jwt.verify(token, secret, (err, userInfo) => {
+		if(!err && userInfo) {
+			callback(undefined, {principalId: userInfo.sub, policyDocument: policy});
+		} else {
+			callback(err);
+		}
+	});
+	
+}
