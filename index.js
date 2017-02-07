73f3cc5e3f350e72c2c67e29e8074562058ccedb
Sander Huijsen <sander.huijsen@gmail.com>
2017-02-07 13:32:34 +0800

    The Auth0 secret is actually not base64 encoded.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const jwt = require('jsonwebtoken');
 if(!process.env.JWT_SECRET) {
-	console.log('Environment variable JWT_SECRET with the base64 encoded secret key is required');
+	console.log('Environment variable JWT_SECRET with secret key is required');
 }
-const secret = new Buffer(process.env.JWT_SECRET, 'base64');
+const secret = new Buffer(process.env.JWT_SECRET);
 const region = process.env.REGION || '*';
 const apiId = process.env.API_ID || '*';
 const stageId = process.env.STAGE_ID || '*';
